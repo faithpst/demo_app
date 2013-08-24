749fa4aee3f4faf1782445d7a6f944c3c58d8874
Wang <wqy874654098@gmail.com>
2013-08-25 04:18:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DmeoApp::Application.config.secret_key_base = '2c8daa9ddfcfbbff9ff7144dfe8f08d20d50d015aeb1f2ec3e90ad6a22c0e55c9b9fd3f4a7b878a6898eb4e458f610ad9d174740da12105f754b465426e009fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DmeoApp::Application.config.session_store :cookie_store, key: '_dmeo_app_session'
